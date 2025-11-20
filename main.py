0b9a2bf0778e81eea6d6eb8034fb730514c0e683
rishSaini <saini.rishabh895@gmail.com>
2025-11-19 20:21:35 -0700

    updates requirements; added error-checking

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from models import URLRequest, SummaryResponse
 from scraper import extract_text_from_url
 from summarizer import summarize_text
+from fastapi import HTTPException
 import json
 
 
@@ -12,7 +13,10 @@ def summarize_article(req: URLRequest):
     text = extract_text_from_url(req.url)
     raw_output = summarize_text(text)
 
-    # raw_output is a JSON string from the LLM, so convert it dict
-    parsed = json.loads(raw_output)
+    try:
+        parsed = json.loads(raw_output)
+    except json.JSONDecodeError:
+        print("Model returned non-JSON:", raw_output)
+        raise HTTPException(status_code=502, detail="Model returned invalid JSON")
 
     return parsed
